7c4ef2575627d65bf98217fedbfc2d1d2fc6b5de
mpa19 <marcperezarnaiz@gmail.com>
2020-03-19 20:08:17 +0100

    added download page web

--- app/src/main/java/com/example/marc/miniact5b/MainActivity.java
+++ app/src/main/java/com/example/marc/miniact5b/MainActivity.java
@@ -12,6 +12,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import android.os.AsyncTask;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView iv;
@@ -20,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn2;
     private ConnectivityManager cM;
     private NetworkInfo nI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         cM = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         nI = cM.getActiveNetworkInfo();
         checkNetwork();
+
     }
 
     private void checkNetwork(){
@@ -52,8 +64,66 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
     }
 
+    class AsynTaskRunner extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            InputStream stream;
+            HttpsURLConnection connection;
+
+            String myurl = "https://www.google.es/";
+            try {
+                URL url = new URL(myurl);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.setReadTimeout(3000);
+                connection.setConnectTimeout(3000);
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+                int response = connection.getResponseCode();
+                if(response != HttpsURLConnection.HTTP_OK)
+                    throw new IOException(("HTTP error code: " + response));
+                stream = connection.getInputStream();
+
+                return readStream(stream, 500);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tv.setText(s);
+        }
+    }
+
     public void onClickText(View v) {
 
+        AsynTaskRunner runner = new AsynTaskRunner();
+        runner.execute();
+    }
+
+    private String readStream(InputStream stream, int i) throws IOException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] rawBuffer = new char[i];
+        int readSize;
+        StringBuffer buffer = new StringBuffer();
+
+        while(((readSize = reader.read(rawBuffer)) != -1) && i >0){
+            if(readSize > 500) {
+                readSize = i;
+            }
+            buffer.append(rawBuffer, 0, readSize);
+            i -= readSize;
+        }
+        return  buffer.toString();
     }
 
     public void onClickImage(View v) {
@@ -64,7 +134,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         checkNetwork();
-
-
     }
 }
